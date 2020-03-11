9e3acc6e05175d29a9bb1de6ac16d684d24f840b
Anthony Giles <omgliekpie@gmail.com>
2020-03-11 16:17:13 -0600

    add some functions for the button and generating the divs

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let clearButton = document.querySelector('header button');
+let rowsAndColumns = '100';
 
 clearButton.addEventListener("mouseenter", button => {
     clearButton.classList.add('overButton');
@@ -6,3 +7,16 @@ clearButton.addEventListener("mouseenter", button => {
 clearButton.addEventListener("mouseleave", button => {
     clearButton.classList.remove('overButton');
 });
+clearButton.addEventListener("click", handleClick);
+
+function handleClick() {
+    rowsAndColumns = +prompt('How many rows and columns?');
+    generateDivs(rowsAndColumns);
+}
+
+function generateDivs(rowsAndColumns) {
+    console.log('generating ' + rowsAndColumns + ' rows and columns');
+    for (i = 0; i <= rowsAndColumns * rowsAndColumns; )
+}
+
+generateDivs(rowsAndColumns);
